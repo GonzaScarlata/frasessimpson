3ec26f576b3a429a211c6dd9a984b12f13dce593
Gonzalo Scarlata <gonza.sc.gs@gmail.com>
2021-04-14 18:55:38 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,34 +1,52 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button'
-import Frase from './components/Frase';
-import {useState, useEffect} from 'react';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Frase from "./components/Frase";
+import { useState, useEffect } from "react";
+import Spinner from "./components/Spinner";
 
 function App() {
-  // crear el state
-  const [personaje, setPersonaje] = useState({});
+    // crear el state
+    const [personaje, setPersonaje] = useState({});
+    const [cargando, setCargando] = useState(false);
 
-  useEffect(()=>{
-    // aqui escribo la logica
-    consultarAPI();
-  }, []);
+    useEffect(() => {
+        // aqui escribo la logica
+        consultarAPI();
+    }, []);
 
-  const consultarAPI = async() =>{
-      const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
-      const resultado = await respuesta.json();
+    const consultarAPI = async () => {
+        setCargando(true);
+        const respuesta = await fetch(
+            "https://thesimpsonsquoteapi.glitch.me/quotes"
+        );
+        const resultado = await respuesta.json();
 
-      console.log(resultado[0]);
-      // Guardar los datos en el state
-      setPersonaje(resultado[0]);
-  }
-
-  return (
-   <section className='container my-5 d-flex flex-column align-items-center'>
-     <img src={process.env.PUBLIC_URL+'logo.png'} alt="logo simpsons" className='w-75 my-4'/>
-     <Button variant="warning" onClick={()=> consultarAPI()}>Obtener frase</Button>
-     <Frase personaje={personaje}></Frase>
-   </section>
-  );
+        console.log(resultado[0]);
+        // Guardar los datos en el state
+        setTimeout(()=>{
+          setPersonaje(resultado[0]);
+          setCargando(false);
+        }, 2000)
+    };
+    //Sintáxis operador ternario :
+    // (condición Lógica) ? (lo que quiero que pase si es true) : (lo que quiero si es false);
+    const mostrarComponente = (cargando) ? (<Spinner />) : (<Frase personaje={personaje}></Frase>);
+    return (
+        <section className="container my-5 d-flex flex-column align-items-center">
+            <img
+                src={process.env.PUBLIC_URL + "logo.png"}
+                alt="logo simpsons"
+                className="w-75 my-4"
+            />
+            <Button variant="warning" onClick={() => consultarAPI()}>
+                Obtener frase
+            </Button>
+            <div className="my-4">
+                {mostrarComponente}
+            </div>
+        </section>
+    );
 }
 
 export default App;
